ce1a34262ffb8c7daf1e142991849386b6df8006
mazenfteha <mazn34903@gmail.com>
2023-03-23 13:53:38 -0700

    connect & disconnect client and work on send messages

--- server.js
+++ server.js
@@ -12,10 +12,25 @@ const io =socketio(server);
 app.use(express.static(path.join(__dirname,'public')))
 
 //run when client folder
-io.on('connection', socket =>{
-    console.log('New WS connection...');
-})
+io.on('connection', socket => {
+
+    //welcom current user
+    socket.emit('message', 'Welcome to chatCord!')
+
+    //Broadcast when a user connects
+    socket.broadcast.emit('message', 'A user has joined the chat')
+
+    // Runs when client disconnects
+    socket.on('disconnect', () => {
+        io.emit('message','A user has left the chat');
+    });
+
+    //listen for chatMessage
+    socket.on('chatMessage', msg =>{
+        io.emit('message',msg) 
+    })
+});
 
 const PORT = 80 || process.env.PORT
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
